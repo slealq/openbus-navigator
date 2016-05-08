1154de423834a8d12bfc798cd5b690212c243741
Stuart Leal Quesada <stuart.leal23@gmail.com>
2016-05-08 01:43:02 -0600

    New file that will replace gpx_parser and the functions inside file_manipulation

---
+++ gpx_manager.py
@@ -0,0 +1,23 @@
+#/usr/share/python
+
+"""
+This file will contain some methods for creating data from a gpx file and creating a gpx file from data. In a future this will substitude the way we create txt files, with the file_manipulation and gpx_parser scripts. Instead this script will use gpxpy, for making the function cleaner.
+"""
+
+# Libraries
+import gpxpy
+
+# Variables
+
+# Helpers
+def gpx2data():
+    """ This should return in some way the data of the gpx file. """
+    pass
+
+def data2gpx():
+    """ This will return a gpx file for posterior writing in disk, from data """
+    pass
+
+# Methods
+
+
